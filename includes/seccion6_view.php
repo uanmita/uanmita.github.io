572b68ebad24b1e61f504ea2663554fb74da12c9
Juanma Parada Espinosa <uanmita@gmail.com>
2025-03-27 14:10:49 +0100

    Subida inicial nueva web

--- includes/seccion6_view.php
+++ includes/seccion6_view.php
@@ -2,8 +2,8 @@
 include 'conexionbd.php';
 
 // Obtener datos de los productos
-$query_seccion5 = $pdo->query("SELECT * FROM seccion WHERE ID=5");
-$seccion5 = $query_seccion5->fetchAll(PDO::FETCH_ASSOC);
+$query_seccion6 = $pdo->query("SELECT * FROM seccion WHERE ID=6");
+$seccion6 = $query_seccion6->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
 <div class="galeria_content w3-justyfied">
