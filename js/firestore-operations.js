90a32a42e0cb95e199259c53627aaa6b1b38e38d
Juanma Parada Espinosa <uanmita@gmail.com>
2025-04-07 11:55:29 +0200

    Configuracion Firebase para login de usuario

--- js/firestore-operations.js
+++ js/firestore-operations.js
@@ -2,7 +2,10 @@
 import { db } from './firebase-config.js';
 import { collection, addDoc, getDocs, doc, updateDoc, deleteDoc } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-firestore.js";
 
-const usersCollection = collection(db, "users");
+const usersCollection = collection(db, "customers");
+
+
+
 
 // Funciones de CRUD...
 // [Incluye aquí el resto del código JavaScript que maneja operaciones con Firestore]
